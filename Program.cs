db09af4233491f958a1afc07202efb13902d4b89
yourjoris <tangajoris@gmail.com>
2024-11-12 16:02:52 +0300

    The starter commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Mr.Benson!");
+
+Console.WriteLine("What operation are you performing?\n" +
+    "1. Record a New Sale\n" +
+    "2. View Monthly Report\n" +
+    "3. Exit");
+
+
+int choice = int.Parse(Console.ReadLine());
+
+if (choice == 1)
+{
+    Console.WriteLine("Which item(s) is being sold?/n" +
+        "1. Bread\n" +
+        "2. Milk\n");
+
+    string nameChoice = Console.ReadLine();
+
+}
+else if (choice == 2)
+{
+
+}
+else if(choice == 3)
+{
+    Console.WriteLine("Goodbye, Mr.Benson!");
+
+}
+else
+{
+    Console.WriteLine("Invalid Choice selected");
+}
